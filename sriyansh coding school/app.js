b35856155cfc317842b1952e09f240961c78e0df
shi-v-ani-singh <shivanisingh1408203@gmail.com>
2025-12-07 00:40:51 +0530

    real and virtual dom

---
+++ sriyansh coding school/app.js
@@ -0,0 +1,31 @@
+// export var a = 20;
+
+
+// export are of 2 types
+
+// 1.default export -send only one particular thing from a file -ES6 version
+// const a = 10;
+// export default a;
+
+
+"---or--"
+// const arr = [10, 20, 30, 40];
+
+// const user = 'Shivani'
+
+// export default arr 
+
+"------------"
+// 2.named export- send multiple things from a file
+// export const arr = [10, 20, 30, 40];
+// const user = 'Shivani'
+
+// export default user 
+
+
+"--------------------------------------"
+const user = 'Shivani'
+export const age = 89
+export const skills = ['Frontend', 'Backend', 'Fullstack']
+
+export default user
--- sriyansh coding school/script.js
+++ sriyansh coding school/script.js
@@ -19,3 +19,35 @@
 "----------------------------------------"
 "-----------IMPORT AND EXPORTS-----------------"
 
+// import a from './app.js'
+
+// console.log(a);
+// this above didnt work so let's see below
+
+// import a from './app.js'
+// console.log(a);
+
+// import array from  './app.js'
+// console.log(array);
+// console.log(user)
+
+// import {arr} from './app.js'
+// console.log(arr);
+
+// import a from './app.js'
+// console.log(a)
+
+"--------------------------------------"
+// import naam from './app.js'
+// console.log(naam);
+
+"--------------------------------------"
+
+import naam from './app.js'
+import user2 from './test.js'
+
+import {age, skills} from './app.js'
+console.log(age, skills)
+
+console.log(naam, user2)
+
