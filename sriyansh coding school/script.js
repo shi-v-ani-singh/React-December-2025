8365114a673afa931a51d1a0486f532530f20785
shi-v-ani-singh <shivanisingh1408203@gmail.com>
2025-12-06 23:48:36 +0530

    saved

---
+++ sriyansh coding school/script.js
@@ -0,0 +1,21 @@
+// var h1 = document.createElement('h1');
+
+// h1.innerHTML = "Hello from JS.";
+
+// document.body.appendChild(h1);
+
+// console.log(h1);
+
+"-----------------------------"
+
+"LIBRARY:-----------"
+//                  - GSAP
+//                  - REACT JS
+
+"FRAMWORKS:------------------------"
+//                   - NEXT JS 
+//                   - ANGULAR JS
+
+"----------------------------------------"
+"-----------IMPORT AND EXPORTS-----------------"
+
