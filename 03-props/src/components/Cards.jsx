017018bf5789e5e2c4851062e2ffcebfad8b1e94
shi-v-ani-singh <shivanisingh1408203@gmail.com>
2025-12-07 21:47:48 +0530

    just uploading updates

---
+++ 03-props/src/components/Cards.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const card = () => {
+  return (
+     <div className="card">
+      <img src="https://images.unsplash.com/photo-1639542270103-0e94fc28be38?q=80&w=816&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+      <h1>Shivani Singh</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+      <button>View Profile</button>
+    </div>
+  )
+}
+
+export default card
