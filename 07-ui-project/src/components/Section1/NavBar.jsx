9c69366feb3ad5d64142a778350a9bc050da5b56
shi-v-ani-singh <shivanisingh1408203@gmail.com>
2025-12-12 23:56:55 +0530

    making-project

---
+++ 07-ui-project/src/components/Section1/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='flex items-center justify-between py-8 px-18'>
+        <h4 className='bg-black uppercase text-white rounded-full px-6 py-2'>Target Audience</h4>
+        <button className='uppercase bg-gray-200 px-6 py-2 rounded-full tracking-wider text-sm'>Digital Dancing Platform</button>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ 07-ui-project/src/components/Section1/Section1.jsx
@@ -0,0 +1,11 @@
+import NavBar from "./NavBar"
+
+const Section1 = () => {
+  return (
+    <div className='h-screen w-full '>
+      <NavBar />
+    </div>
+  )
+}
+
+export default Section1
